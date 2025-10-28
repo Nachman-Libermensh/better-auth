8a5733d8226e4562506cf66409c2086210bd16f4
Nachman Libermensh <149395370+Nachman-Libermensh@users.noreply.github.com>
2025-10-28 20:49:47 +0200

    Align dialog layouts for RTL

--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -46,6 +46,7 @@ function AlertDialogOverlay({
 
 function AlertDialogContent({
   className,
+  dir = "rtl",
   ...props
 }: React.ComponentProps<typeof AlertDialogPrimitive.Content>) {
   return (
@@ -57,6 +58,7 @@ function AlertDialogContent({
           "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg",
           className
         )}
+        dir={dir}
         {...props}
       />
     </AlertDialogPortal>
@@ -70,7 +72,7 @@ function AlertDialogHeader({
   return (
     <div
       data-slot="alert-dialog-header"
-      className={cn("flex flex-col gap-2 text-center sm:text-left", className)}
+      className={cn("flex flex-col gap-2 text-right", className)}
       {...props}
     />
   )
